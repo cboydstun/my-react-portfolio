240ef7d90d07097a450e2ee4191ae66237966073
Chris Boydstun <chrisboydstun@gmail.com>
2020-02-12 16:16:06 -0600

    Updates

---
+++ precache-manifest.79910334231e2e8a522065d502c2e66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59e19fc407a655a403b66c91e071c19",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "450ce3cbc045bc0e533b",
+    "url": "/my-react-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1ebeb0c6ef8dffa63375",
+    "url": "/my-react-portfolio/static/js/2.6931cec2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-react-portfolio/static/js/2.6931cec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450ce3cbc045bc0e533b",
+    "url": "/my-react-portfolio/static/js/main.be663233.chunk.js"
+  },
+  {
+    "revision": "4f17dc5f8367dff5007d",
+    "url": "/my-react-portfolio/static/js/runtime-main.6ed9587e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
