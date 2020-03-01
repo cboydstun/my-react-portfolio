ef9ec7ff0ecfd4412f089cbcb74529a1b3e4f31d
Chris Boydstun <chrisboydstun@gmail.com>
2020-02-29 23:15:25 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">Hello World!</header>
+      <Button>Button</Button>
     </div>
   );
 }
