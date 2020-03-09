c94e74435ddd5dfd3e24e76f6a7f96df57569707
Chris Boydstun <chrisboydstun@gmail.com>
2020-03-09 18:57:34 -0500

    Updates

---
+++ precache-manifest.6fe8e621e7fcbaac10e2a3d7e6ebb003.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ad121b81f2282a5e3169a8277c0686",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "350612e8c6ad6893daf6",
+    "url": "/my-react-portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2ba7d8acadd29c987f08",
+    "url": "/my-react-portfolio/static/css/main.ddc34966.chunk.css"
+  },
+  {
+    "revision": "350612e8c6ad6893daf6",
+    "url": "/my-react-portfolio/static/js/2.390bebe5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-react-portfolio/static/js/2.390bebe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba7d8acadd29c987f08",
+    "url": "/my-react-portfolio/static/js/main.a95d54a4.chunk.js"
+  },
+  {
+    "revision": "4f17dc5f8367dff5007d",
+    "url": "/my-react-portfolio/static/js/runtime-main.6ed9587e.js"
+  },
+  {
+    "revision": "2a881db159d02a2cd541ec902e55616b",
+    "url": "/my-react-portfolio/static/media/MyPic.2a881db1.jpg"
+  }
+]);
